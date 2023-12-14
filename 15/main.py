85523cb046aae8b32cbf5951e0ff972453fbc047
sweetestsj <sj.sweetest@gmail.com>
2023-12-14 13:29:39 -0600

    project 15

--- 15/main.py
+++ 15/main.py
@@ -32,20 +32,61 @@ resources = {
 }
 
 
-def is_resource_sufficient():
+def is_resource_sufficient(selected_menu: str) -> bool:
     """Returns True when order can be made, False if ingredients are insufficient."""
-    pass
+    ingredients = ['water','milk','coffee']
+    for ingredient in ingredients:
+        if ingredient in MENU[selected_menu]['ingredients'] and MENU[selected_menu]['ingredients'][ingredient] > resources[ingredient]:
+            print(f"insufficient {ingredient}, remaining: {resources[ingredient]}, required for {selected_menu}: {MENU[selected_menu]['ingredients'][ingredient]}")
+            return False
+    return True
 
-
-def process_payment():
+def process_payment() -> float:
     """Returns the total payment received and issue change."""
-    pass
+    payment = 0.0
+    price = MENU[selected_menu]['cost']
+    while payment < price:
+        print(f"Price for {selected_menu}: {price}. You paid {payment}. Remaining: {price - payment}")
+        payment += float(input("​Make payment\n"))
+    return payment - price
 
-def make_coffee():
+def make_coffee(selected_menu: str):
     """Deduct the required ingredients from the resources."""
-    pass
+    ingredients = ['water','milk','coffee']
+    for ingredient in ingredients:
+        if ingredient in MENU[selected_menu]['ingredients']:
+            resources[ingredient] -= MENU[selected_menu]['ingredients'][ingredient]
 
 
 while True:
-    choice = input("​What would you like? (espresso/latte/cappuccino): ")
-    break
+    print(f"\nAdmin View")
+    print(f"Profit: {profit}")
+    print(f"Menu: {MENU}")
+    print(f"resources: {resources}\n")
+    print("Menu Options: ")
+    print(f"0 -> Espresso: {MENU['espresso']['cost']}")
+    print(f"1 -> Latte: {MENU['latte']['cost']}")
+    print(f"2 -> Cappuccino: {MENU['cappuccino']['cost']}")
+    choice = int(input("​What would you like? (0: espresso / 1: latte/ 2: cappuccino): \n"))
+    selected_menu = ""
+    match choice:
+        case 0:
+            selected_menu = 'espresso'
+        case 1:
+            selected_menu = 'latte'
+        case 2:
+            selected_menu = 'cappuccino'
+        case _:
+            print("Invalid menu choice, click enter to continue")
+    can_make = is_resource_sufficient(selected_menu)
+    if not can_make:
+        print("Resource insufficient, click enter to continue")
+        continue
+    change = process_payment()
+    make_coffee(selected_menu)
+
+    profit += MENU[selected_menu]['cost']
+    print(f"Change issued: {change}")
+    print(f"Profit added: {MENU[selected_menu]['cost']}, total profit: {profit}")
+    print("Click enter to continue")
+
